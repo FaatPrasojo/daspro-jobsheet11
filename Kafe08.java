64f5bf6573e6d838f7bfa3794c4bd8cd9d752982
FaatPrasojo <faatprasojo@gmail.com>
2024-11-21 01:45:45 +0700

    Percobaan 2 Prasojo

--- Kafe08.java
+++ Kafe08.java
@@ -1,6 +1,11 @@
-import java.util.Scanner;
 public class Kafe08 {
-   public static void Menu() {
+   public static void Menu(String namaPelanggan, boolean isMember, String kodePromo) {
+    System.out.println("Selamat datang, " + namaPelanggan + "!");
+
+    if (isMember) {
+        System.out.println("Anda adalah member, dapatkan diskon 10% untuk setiap pembelian!");
+    }
+
     System.out.println("===== MENU RESTO KAFE =====");
     System.out.println("1. Kopi Hitam - Rp 15,000");
     System.out.println("2. Cappuccino - Rp 20,000");
@@ -10,8 +15,19 @@ public class Kafe08 {
     System.out.println("6. Mie Goreng - Rp 18,000");
     System.out.println("===========================");
     System.out.println("Silakan pilih menu yang anda inginkan.");
+   
+        switch (kodePromo) {
+            case "DISKON50":
+                System.out.println("Anda mendapatkan diskon 50%");
+                break;
+            case "DISKON30":
+                System.out.println("Anda mendapatkan diskon 30%");
+                break;
+            default:
+                System.out.println("Kode promo tidak valid");
+        }
    }
     public static void main(String[] args) {
-        Menu();
+        Menu("Andi", true, "DISKON50");
     }
 }
